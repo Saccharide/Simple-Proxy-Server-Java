f734066d010f7762541752e0f3f38ecb72ce748c
saccharide <rtang@email.wm.edu>
2018-04-22 18:17:56 -0400

    Added very detailed explanation of what the code is doing

--- ProxyThread.java
+++ ProxyThread.java
@@ -12,16 +12,33 @@ public class ProxyThread extends Thread {
         this.socket = socket;
     }
 
+    /*****************************************************/
+    /* A simple proxy server needs to do the following:  */
+    /*     1) Get     request  from client               */
+    /*     2) Forward request  to   server               */
+    /*     3) Get     response from server               */
+    /*     4) Forward response to   client               */
+    /*****************************************************/
     public void run() {
-        
+
         try {
+            // Setting the input stream and output stream for future analysis.
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // Storing client request
             String inputLine;
-    
+
+            // Setting a flag to see if we are on the first line
             boolean first_line_reached = false;
+            
+            // Initialize the url we will get from client socket
             String server_url = "";
 
+            /*******************************/
+            /* Step 1                      */
+            /* Getting request from client */
+            /*******************************/
             while ((inputLine = in.readLine()) != null) {
                 try {
                     StringTokenizer tok = new StringTokenizer(inputLine);
@@ -29,32 +46,47 @@ public class ProxyThread extends Thread {
                 } catch (Exception e) {
                     break;
                 }
+                // Analyze the first line of the network request from socket to find the url and protocol
                 if (!first_line_reached) {
                     String[] tokens = inputLine.split(" ");
                     if (tokens.length > 0 )
                         server_url = tokens[1];
                     else {
+//                        socket.close();
                         return;
                     }
-                    System.out.println("--------------------------");
+                    System.out.println("-----------------------------------------------");
                     for(int i = 0; i < tokens.length; i++)
                         System.out.println(tokens[i]);
+
                     break;
                 }
             }
-
-
+            /* Step 1 complete                    */
+            /* Finish getting request from client */
+            
             BufferedReader rd = null;
             try {
                 System.out.println("Making connection to : " + server_url);
 
+                /***************************/
+                /* Step 2                  */
+                /* Send request to server  */
+                /***************************/
+                
                 URL url = new URL(server_url);
                 URLConnection conn = url.openConnection();
-
                 conn.setDoInput(true);
+                /* Step 2 complete                    */
+                /* Finish sending request to server   */
 
+                /*****************************/
+                /* Step 3                    */
+                /* Get response from server  */
+                /*****************************/
+                
                 InputStream is = null;
-                HttpURLConnection huc = (HttpURLConnection)conn;
+                
                 if (conn.getContentLength() > 0) {
                     try {
                         is = conn.getInputStream();
@@ -66,7 +98,15 @@ public class ProxyThread extends Thread {
                 else{
                     socket.close();
                 }
+                /* Step 3 complete                     */
+                /* Finish getting response from server */
 
+
+                /***************************/
+                /* Step 4                  */
+                /* Send response to client */
+                /***************************/
+                
                 byte by[] = new byte[ BUFFER_SIZE ];
                 if (is != null) {
                     int index = is.read(by, 0, BUFFER_SIZE);
@@ -76,11 +116,19 @@ public class ProxyThread extends Thread {
                     }
                     out.flush();
                 }
+                
+                /* Step 4 complete                     */
+                /* Finish sending response to client   */
 
             } catch (Exception e) {
+                //can redirect this to error log
+                System.err.println("Encountered exception: " + e);
+                //encountered error - just send nothing back, so
+                //processing can continue
                 out.writeBytes("");
             }
 
+            //close all resources
             if (rd != null) {
                 rd.close();
             }
@@ -100,7 +148,7 @@ public class ProxyThread extends Thread {
     }
 
     private boolean isHTTP (String url){
-        
+
         if (url.substring(7).equals("http://"))
             return true;
         else{
@@ -113,7 +161,7 @@ public class ProxyThread extends Thread {
                     return true;
             }
         }
-        
+
     }
 
 }
