afa2f97dfeff7e002379199cf59e8d7b2932c887
saccharide <rtang@email.wm.edu>
2018-04-22 18:20:29 -0400

    Removed some unnecessary code and added some comments

--- ProxyServer.java
+++ ProxyServer.java
@@ -18,34 +18,15 @@ public class ProxyServer extends AsyncTask<Void, Void, Void> {
             System.out.println("Started on: " + port);
         } catch (IOException e) {
             System.err.println("Could not listen on port: " + port);
-            System.exit(-1);
+            return null;
         }
-        String[] URLs = new String[10000];
-        int index = 0;
+
             try {
                 Socket socket;
+                // Start listening to our port
                 while ((socket = ServerSocket.accept()) != null) {
 
-//                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                String URLtoCall = "";
-//                String inputLine = in.readLine();
-//                if ( inputLine != null){
-//                    String[] tokens = inputLine.split(" ");
-//                    URLtoCall = tokens[1];
-//                }
-//                if (Arrays.asList(URLs).contains(URLtoCall)) {
-//                    System.out.println("URL exists!");
-//                    new ProxyResponseThread(socket, URLtoCall).start();
-//                    continue;
-//                }
-//                URLs[index] = URLtoCall;
-//                index++;
-//                SocketAddress address =socket.getRemoteSocketAddress();
-//                String url=((InetSocketAddress)address).getHostString();
                     new ProxyThread(socket).start();
-//                      new ProxyThread(socket).start();
-//
-//                new ProxyResponseThread(socket, url).start();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
